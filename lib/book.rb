aa3278886eccdb48b7a0490c088553842e96fac0
Allegra <aukseallegra@gmail.com>
2018-06-07 09:14:54 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -18,21 +18,21 @@ class Book
     @author 
   end 
   
-  def page_count=(num)   #setter
-    @page_count = num 
-  end 
+  # def page_count=(num)   #setter
+  #   @page_count = num 
+  # end 
   
-  def page_count   #getter 
-    @page_count
-  end 
+  # def page_count   #getter 
+  #   @page_count
+  # end 
   
-  def genre=(genre_name)   #setter
-    @genre = genre_name
-  end 
+  # def genre=(genre_name)   #setter
+  #   @genre = genre_name
+  # end 
   
-  def genre   #getter 
-    @genre
-  end 
+  # def genre   #getter 
+  #   @genre
+  # end 
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
