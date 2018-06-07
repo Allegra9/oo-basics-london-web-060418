6fa413ef7db6f40e0a37cc41c48626ed25d34e9c
Allegra <aukseallegra@gmail.com>
2018-06-07 09:23:44 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   
-  # attr_accessor :author, :page_count, :genre 
-   attr_reader :brand 
+  attr_accessor :color, :size, :genre 
+  attr_reader :brand 
   
   def initialize(brand)
     @brand = brand
