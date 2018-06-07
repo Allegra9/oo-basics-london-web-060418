f0c1c8675b64877fd2a648e7a0090f45916e3e51
Allegra <aukseallegra@gmail.com>
2018-06-07 09:25:14 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
   
-  attr_accessor :color, :size, :material, :condition, :cobble
+  attr_accessor :color, :size, :material, :condition 
   attr_reader :brand 
   
   def initialize(brand)
     @brand = brand
   end 
   
-  def turn_page
+  def cobble
     puts "Flipping the page...wow, you read fast!"
   end 
   
